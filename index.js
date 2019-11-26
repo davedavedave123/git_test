0e0f7328be98bc7d737d896779a2f92ce4161e89
Dave <davebaxtermusic@gmail.com>
2019-11-27 10:08:41 +1300

    added a button

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const root = document.getElementById("root");
 const text = "hi this is a message";
 const node = document.createTextNode(text);
 
+function click() {
+    alert(":)")
+}
+
+const button = document.createElement("button");
+button.innerHTML = "click here";
+button.onclick = () => click()
+
+
+
 root.appendChild(node);
+root.appendChild(button);
